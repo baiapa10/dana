4b340335ac2db8e8dd6168d7c406150b48d532fb
Farhan Izzuadi <rfir06@gmail.com>
2026-02-23 16:52:54 +0700

    tst

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const Controller = require('../controllers/controller')
 const router = require('express').Router()
 
 
-// router.get('/', Controller.home)
+router.get('/', Controller.home)
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router()
 const user = require('./user.js')
 const merchant = require('./merchant.js')
 const login = require('./login.js')
+const home = require('./home.js')
+
 const Controller = require('../controllers/controller.js')
 
 // router.get('/', Controller.home)
-router.use("/homes", login)
+router.use("/homes", home)
 router.use("/users", user)
 router.use("/merchants", merchant)
 
